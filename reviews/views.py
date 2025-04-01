aa6048332b3004a05e4eb56964c48b1f52dab334
9637231757 <er.rohangaidhani@gmail.com>
2025-04-01 05:55:39 +0530

    formview

--- reviews/views.py
+++ reviews/views.py
@@ -2,35 +2,23 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView 
 
 from .forms import ReviewForm
 from .models import Review
 # using class
 
-class ReviewView(View):
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    sucess_url = "/thank-you"
     
-    def get(self, request):
-        form = ReviewForm()     
-         
-        return render(request, "reviews/review.html", {
-            "form": form
-            #"has_error": False
-        })
-
-              
-    def post(self, request):
-        form = ReviewForm(request.POST)  
-        
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-                     
-        return render(request, "reviews/review.html", {
-            "form": form
-           #"has_error": False
-        })
-
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+    
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
     
@@ -42,19 +30,15 @@ class ThankYouView(TemplateView):
 class ReviewsListView(ListView):
     template_name = "reviews/review_list.html"
     model = Review
+    context_object_name = "reviews"
     
       
         
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html" 
+    model = Review
    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id =kwargs["id"]
-        selected_review = Review.objects.get(pk=review_id)
-        reviews = Review.objects.all()
-        context["reviews"] = selected_review
-        return context
+ 
        
         
          
