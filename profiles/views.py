e3ab2e54fb6e86681f27c55906e9d318e5afc3c1
9637231757 <er.rohangaidhani@gmail.com>
2025-04-01 17:57:59 +0530

    createview

--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,25 @@
-"""from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # Create your views here.
 from django.views import View
 from .forms import ProfileForm
 from .models import UserProfile
 from .forms import ProfileForm
-# Create your views here.
+from django.views.generic.edit import CreateView
 
-def store_files(file):
+"""def store_files(file):
     with open("temp/image.jpg","wb+") as dest:
         for chunk in file.chunks():
-            dest.write(chunk)
+            dest.write(chunk)"""
+
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"            
             
           
-class CreateProfileView(View):
+"""class CreateProfileView(View):
     def get(self, request):
         form = ProfileForm()
         return render(request, "profiles/create_profile.html",{
@@ -31,29 +37,4 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html", {
             "form":submitted_form
         })"""
-        
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.views import View
-from .forms import ProfileForm
-from .models import UserProfile
-
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
-        
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-        
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })        
         
